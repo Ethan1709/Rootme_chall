37ed00177d8973d9d9af2656aba320081048ef0b
Ethan1709 <benyayerethan@gmail.com>
2023-10-03 14:13:59 +0200

    adding captcha challenge

--- Programmation/captcha.py
+++ Programmation/captcha.py
@@ -1,22 +1,34 @@
 import base64
-import requests
+import pytesseract
+import base64
+from PIL import Image
+from io import BytesIO
+from selenium import webdriver
 
-# URL of the CAPTCHA image
-captcha_url = "http://challenge01.root-me.org/programmation/ch8/captcha.php"
+captcha_url = "http://challenge01.root-me.org/programmation/ch8/"
 
-# Request the CAPTCHA image
-response = requests.get(captcha_url)
-captcha_data = response.content
+driver = webdriver.Firefox()
 
-# Decode the base64 image data
-captcha_image = base64.b64decode(captcha_data)
+driver.get(captcha_url)
 
-# Save the CAPTCHA image to a file (optional)
-with open("captcha.png", "wb") as image_file:
-    image_file.write(captcha_image)
+for i in range(0, 100):
 
-# TODO: Implement your CAPTCHA solving logic here
-# You can use libraries like OpenCV or Tesseract to process and solve the CAPTCHA image
+    input_field = driver.find_element("name", "cametu")
+    submit_button = driver.find_element("xpath", "//input[@value='Try']")
 
-# Example: Print the base64 data of the CAPTCHA image
-print(captcha_data)
+    response_content = driver.page_source.encode("utf-8").decode("utf-8")
+    split_content = response_content.split('"')
+    captcha_image = split_content[17][22:]
+
+    try:
+        captcha_bytes = base64.b64decode(captcha_image)
+        image_stream = BytesIO(captcha_bytes)
+        captcha_image = Image.open(image_stream)
+        captcha_text = pytesseract.image_to_string(captcha_image)
+        print(captcha_text)
+        input_field.send_keys(captcha_text)
+        submit_button.click()
+    except Exception as e:
+        print(f"Error: {e}")
+
+
